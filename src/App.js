1ebe382b5af74d11db3b70e638237ea55f4c79a9
Millina <millina.lehikoinen@gmail.com>
2023-11-24 20:04:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>PolicyBot</p>
+      </header>
+      <body className="App-body">
+        <p>Upload your traffic file</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
