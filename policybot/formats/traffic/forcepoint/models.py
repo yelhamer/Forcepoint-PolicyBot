cb85daf9665d99eb46b486c204cb2ff83aa9a330
Yacine Elhamer <elhamer.yacine@gmail.com>
2023-12-08 22:10:05 +0100

    policybot.formats.traffic: added abstract classes and interfaces for unifying operations on different vendors' traffic logs

--- policybot/formats/traffic/forcepoint/models.py
+++ policybot/formats/traffic/forcepoint/models.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, field_validator
 from pydantic.networks import IPvAnyAddress
 from datetime import datetime
-from typing import Optional, List
+from typing import List
 
 class ForcePointLogEntry(BaseModel):
     Timestamp: datetime
+    ReceptionTime: datetime
     LogId: int
     NodeId: str
     Facility: str
@@ -13,14 +14,13 @@ class ForcePointLogEntry(BaseModel):
     Action: str
     Src: IPvAnyAddress
     Dst: IPvAnyAddress
-    Service: str
-    Protocol: str
     Sport: int
     Dport: int
+    Service: str
+    Protocol: str
     RuleId: float
     Srcif: int
     SrcVlan: int
-    ReceptionTime: datetime
     SenderType: str
     SituationId: int
     Situation: str
@@ -28,11 +28,11 @@ class ForcePointLogEntry(BaseModel):
 
     @field_validator('Sport', 'Dport')
     @classmethod
-    def validate_port(cls, p: int):
-        if p not in range(0, 65536):
-            raise ValueError(f"{p} is not a valid port")
-        return p
-    
+    def validate_port(cls, port: int):
+        if port not in range(0, 65536):
+            raise ValueError(f"{port} is not a valid port number.")
+        return port
 
-class ForcePointLog(BaseModel):
-    entries: Optional[List[ForcePointLogEntry]] = None
+
+class ForcePointTrafficLog(BaseModel):
+    entries: List[ForcePointLogEntry]
