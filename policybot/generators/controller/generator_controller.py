f6cc67b3f8fbcd27cafc5b8aa36f097c0c670f20
junaid-sahl <48208703+junaid-sahl@users.noreply.github.com>
2024-02-29 12:24:50 +0200

    Generate XML using ElementTree

---
+++ policybot/generators/controller/generator_controller.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.openapi.models import Response
+from fastapi.responses import JSONResponse
+import json
+from fastapi import APIRouter
+
+from policybot.generators.service.policy_generator import generate_policy
+
+router = APIRouter()
+
+
+@router.post("/generator/")
+async def create_upload_file(rules):
+    try:
+        xml_data = generate_policy(rules)
+        headers = {
+            'Content-Type': 'application/xml',
+            'Content-Disposition': 'attachment; filename=output.xml'
+        }
+
+        return Response(xml_data, headers=headers)
+    except Exception as e:
+        return JSONResponse(content={"detail": f"Error processing the rules: {str(e)}"}, status_code=400)
