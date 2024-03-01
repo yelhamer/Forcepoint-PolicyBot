3a7050ea21d3fab32c7e38f2c849ed3c13830275
Yacine <16624109+yelhamer@users.noreply.github.com>
2024-03-01 16:36:23 +0200

    removed formData

--- frontend/src/uploadJson.js
+++ frontend/src/uploadJson.js
@@ -1,8 +1,4 @@
 export default async function uploadJson(file) {
-  const formData = new FormData();
-
-  console.log(file.text());
-  formData.set('file', file);
   const response = await fetch('http://127.0.0.1:8000/upload/traffic/forcepoint/', {
     method: "POST",
     headers: {'Content-Type': 'application/json'},
