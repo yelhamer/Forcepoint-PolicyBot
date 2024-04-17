cbe6b3a7498378f62ba81d5fe04f7041e2196244
Amy Nymalm <amy.a.nymalm@utu.fi>
2024-04-17 20:28:00 +0300

    added right rules

--- frontend/src/networkPorts.js
+++ frontend/src/networkPorts.js
@@ -1,7 +1,7 @@
 
 const networkPorts = [
   //Any port/service pairs to be shown in the drop down menu for ports can be added here
-  ['Any', 'Any'],
+  ['Any', 0],
   ['FTP', 21],
   ['HTTP', 80],
   ['SSH', 22],
